d16fdd695688064d2fb22754fd79fe3ea1c539c9
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2023-05-24 13:07:11 +0300

    added open file dialog

--- Alex J. Star Detection/Gui.py
+++ Alex J. Star Detection/Gui.py
@@ -1,37 +1,96 @@
 from tkinter import *
+from tkinter import filedialog
+
 import tkinterDnD as tkD
 import tkinterdnd2
 from PIL import ImageTk, Image
 
 
-def DropImage(event):
-    testvariable.set(event.data)
-    # get the value from string variable
-    window.file_name = testvariable.get()
-    # takes path using dragged file
-    image_path = Image.open(str(window.file_name))
-    # resize image
-    reside_image = image_path.resize((200, 200), Image.ANTIALIAS)
-    # displays an image
-    window.image = ImageTk.PhotoImage(reside_image)
-    image_label = Label(labelframe, image=window.image).pack()
+class CoinApp:
+    def __init__(self):
+        self.root = tkD.Tk()
+        self.config_root()
+        # main frame
+        self.testvariable = StringVar()
+        self.labelframe = LabelFrame(self.root, bg='gold')
+        self.entrybox = Entry(self.root, textvar=self.testvariable, width=80)
+        self.textlabel = Label(self.root, text='drop the file below', bg='#fcba03')
+        self.main_frame_config()
+        # menubar
+        self.init_menu()
+
+    def config_root(self):
+        self.root.title('Alexander Jannaeus Coin Classifier')
+        self.root.geometry('400x300')
+        self.root.config(bg='gold')
+
+    def init_menu(self):
+        menubar = Menu(self.root)
+        self.root.config(menu=menubar)
+
+        file_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label='File', menu=file_menu)
+        file_menu.add_command(label='Open', command=self.open_file)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=self.root.quit)
+        menubar.add_cascade(label='Help', command=self.help_window)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(title='Select Image File',
+                                               filetypes=[('Image Files', '*.png *.jpg *.jpeg')])
+        if file_path:
+            self.testvariable.set(file_path)
+            self.show_image()
+
+    def help_window(self):
+        # Toplevel object which will
+        # be treated as a new window
+        help_win = Toplevel(self.root)
+
+        # sets the title of the
+        # Toplevel widget
+        help_win.title("Help")
+
+        # sets the geometry of toplevel
+        help_win.geometry("300x300")
+
+        # A Label widget to show in toplevel
+        Label(help_win,
+              text="Here's a guide how to use the program").pack()
+        text = Label(help_win)
+        text.config(text="DISABLED")
+        text.pack()
+
+    def main_frame_config(self):
+        self.textlabel.pack(anchor=NW, padx=10)
+
+        self.entrybox.pack(fill=X, padx=10)
+        self.entrybox.drop_target_register(tkinterdnd2.DND_FILES)
+        self.entrybox.dnd_bind('<<Drop>>', self.drop_image)
+        self.labelframe.drop_target_register(tkinterdnd2.DND_FILES)
+        self.labelframe.dnd_bind('<<Drop>>', self.drop_image)
+
+        self.labelframe.pack(fill=BOTH, expand=True, padx=9, pady=9)
+
+    def drop_image(self, event):
+        self.testvariable.set(event.data)
+        self.show_image()
+
+    def show_image(self):
+        # get the value from string variable
+        self.root.file_name = self.testvariable.get()
+
+        image_path = Image.open(str(self.root.file_name))
+        # resize image
+        reside_image = image_path.resize((200, 200), Image.ANTIALIAS)
+        # displays an image
+        self.root.image = ImageTk.PhotoImage(reside_image)
+        image_label = Label(self.labelframe, image=self.root.image).pack()
+
+    def run(self):
+        self.root.mainloop()
 
 
-window = tkD.Tk()
-window.title('Alexander Jannaeus Coin Classifier')
-window.geometry('400x300')
-window.config(bg='gold')
-
-testvariable = StringVar()
-textlabel = Label(window, text='drop the file below', bg='#fcba03')
-textlabel.pack(anchor=NW, padx=10)
-entrybox = Entry(window, textvar=testvariable, width=80)
-entrybox.pack(fill=X, padx=10)
-entrybox.drop_target_register(tkinterdnd2.DND_FILES)
-entrybox.dnd_bind('<<Drop>>', DropImage)
-
-labelframe = LabelFrame(window, bg='gold')
-
-labelframe.pack(fill=BOTH, expand=True, padx=9, pady=9)
-
-window.mainloop()
+if __name__ == '__main__':
+    app = CoinApp()
+    app.run()
--- Alex J. Star Detection/README.md
+++ Alex J. Star Detection/README.md
@@ -1,14 +1,22 @@
 ## TODO
-> **legend** <br> ✅ - done <br>
+> **legend** <br> 
+✅ - done <br>
 🔳 - needs to be done<br>
 💬 - in progress<br>
 
 ✅ Write function to append data files from data/6 and data/8 and data/0 to data.csv in Data.py<br>
 ✅ Write function to take X random samples from train dir and transfer to test dir. Data.py<br>
 ✅ Write function to take all samples in test dir back to train dir Data.py <br>
-🔳 Train models overnight, saved based on success rate on test <br>
+💬 Train models overnight, saved based on success rate on test <br>
 💬 GUI window to drag file into and get prediction based on Predictor.py <br>
 - [x] drag and drop option
-- [ ] open dialog option
-- [ ] prettier design 😩<br>
+- [ ] menu
+  - [x] open dialog option
+  - [ ] help/guide button
+- [ ] predictor (automatic/by button)
+- [ ] multiple files?
+  - [ ] clear files
+  - [ ] load already brought on files
+- [ ] resize/crop option https://www.tutorialspoint.com/opencv-python-how-to-draw-a-rectangle-using-mouse-events
+- [ ] prettier design 😩
 <br>
