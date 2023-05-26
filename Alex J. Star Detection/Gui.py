e653fcea6d05eed5bf0741761543cbc46d8662f5
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2023-05-26 19:40:14 +0300

    added predict button

--- Alex J. Star Detection/Gui.py
+++ Alex J. Star Detection/Gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 
+# from Predictor import multi_predict
 import tkinterDnD as tkD
 import tkinterdnd2
 from PIL import ImageTk, Image
@@ -14,14 +15,16 @@ class CoinApp:
         self.testvariable = StringVar()
         self.labelframe = LabelFrame(self.root, bg='gold')
         self.entrybox = Entry(self.root, textvar=self.testvariable, width=80)
-        self.textlabel = Label(self.root, text='drop the file below', bg='#fcba03')
+        self.textlabel = Label(self.root, text='drop the file below')
+        self.predition_label = Label(self.root, text='Label: ')
+        self.predict_btn = Button(self.root, text="predict", command=self.predict)
         self.main_frame_config()
         # menubar
         self.init_menu()
 
     def config_root(self):
         self.root.title('Alexander Jannaeus Coin Classifier')
-        self.root.geometry('400x300')
+        self.root.geometry('700x600')
         self.root.config(bg='gold')
 
     def init_menu(self):
@@ -43,23 +46,15 @@ class CoinApp:
             self.show_image()
 
     def help_window(self):
-        # Toplevel object which will
-        # be treated as a new window
         help_win = Toplevel(self.root)
 
-        # sets the title of the
-        # Toplevel widget
         help_win.title("Help")
-
-        # sets the geometry of toplevel
-        help_win.geometry("400x300")
-
-        # A Label widget to show in toplevel
+        help_win.geometry("500x300")
         Label(help_win,
               text="Here's a guide how to use the program").pack()
         text = Text(help_win, wrap=WORD)
-        text.insert(INSERT, "1. Choose a picture for inspection by- \n")
-        text.insert(INSERT, "\t • Dragging it onto the window \n \t • By file > open\n")
+        text.insert(INSERT, "1. Choose a picture for inspection by one of the options- \n")
+        text.insert(INSERT, "\t • Dragging it onto the window \n \t • file > open\n")
         text.insert(INSERT, "2. Edit the picture by cropping the star side if needed\n")
         text.insert(INSERT, "3. Press the classify button \n")
         text.insert(INSERT, "4. A label will appear with the approximate probability\n")
@@ -68,14 +63,20 @@ class CoinApp:
 
     def main_frame_config(self):
         self.textlabel.pack(anchor=NW, padx=10)
-
         self.entrybox.pack(fill=X, padx=10)
         self.entrybox.drop_target_register(tkinterdnd2.DND_FILES)
         self.entrybox.dnd_bind('<<Drop>>', self.drop_image)
         self.labelframe.drop_target_register(tkinterdnd2.DND_FILES)
         self.labelframe.dnd_bind('<<Drop>>', self.drop_image)
-
         self.labelframe.pack(fill=BOTH, expand=True, padx=9, pady=9)
+        self.predict_btn.config(state=DISABLED)
+        self.predict_btn.pack()
+        self.predition_label.pack()
+
+    def predict(self):
+        image_path = str(self.root.file_name)
+        res = "ass"
+        self.predition_label.config(text="Label: " + res)
 
     def drop_image(self, event):
         self.testvariable.set(event.data)
@@ -87,10 +88,11 @@ class CoinApp:
 
         image_path = Image.open(str(self.root.file_name))
         # resize image
-        reside_image = image_path.resize((200, 200), Image.ANTIALIAS)
+        # reside_image = image_path.resize((200, 200), Image.ANTIALIAS)
         # displays an image
-        self.root.image = ImageTk.PhotoImage(reside_image)
+        self.root.image = ImageTk.PhotoImage(image_path)
         image_label = Label(self.labelframe, image=self.root.image).pack()
+        self.predict_btn.config(state=NORMAL)
 
     def run(self):
         self.root.mainloop()
